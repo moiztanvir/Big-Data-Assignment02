0d998f4636a63c7e3eb8edd729c34f2eec0c77aa
Moiz Tanvir <151388609+moiztanvir@users.noreply.github.com>
2024-03-29 15:59:47 +0500

    mapper_a1.py

--- mapper_a1.py
+++ mapper_a1.py
@@ -7,14 +7,11 @@ import re
 word_pattern = re.compile(r'\b\w+\b')
 
 # Initialize a dictionary to store the term frequency for each ARTICLE_ID
-term_frequency = {}
 t2 = {}
 # Read input from standard input (stdin)
 reader = csv.reader(sys.stdin)
-i = 0
-j = 0
 next(reader)
-
+t = []
 # Iterate over each row in the CSV file
 for row in reader:
     if len(row) == 2:
@@ -23,13 +20,14 @@ for row in reader:
         s = section_text.split()
         s = sorted(s)
         for word in s:
+            if word not in t:
+                t.append(word)
+        for word in s:
             if s.count(word)>1:
                 if word not in m:
-                    m.add((i, s.count(word)))
+                    m.add((t.index(word), s.count(word)))
             else:
-                term_frequency[i] = word
-                m.add((i, 1))
-                i = i+1
+                m.add((t.index(word), 1))
         t2[article_id] = m
 
 
